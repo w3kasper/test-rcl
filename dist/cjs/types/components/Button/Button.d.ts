9178dd202ee888d5d02023717f11eeaf3643e006
w3kasper <hellovikram1@gmail.com>
2024-03-19 08:47:08 -0600

    first commit

---
+++ dist/cjs/types/components/Button/Button.d.ts
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Button.css";
+interface ButtonProps {
+    label: string;
+}
+declare const Button: (props: ButtonProps) => React.JSX.Element;
+export default Button;
